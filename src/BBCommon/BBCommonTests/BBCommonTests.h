7541e19722c4bba6c787ff59007ce263e8c573df
Brian Romanko <brian@bigbigbomb.com>
2011-09-18 11:35:53 -0500

    Adding project template

---
+++ src/BBCommon/BBCommonTests/BBCommonTests.h
@@ -0,0 +1,17 @@
+//
+//  BBCommonTests.h
+//  BBCommonTests
+//
+//  Created by Brian Romanko on 9/18/11.
+//  Copyright 2011 BigBig Bomb, LLC. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface BBCommonTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ src/BBCommon/BBCommonTests/BBCommonTests.m
@@ -0,0 +1,33 @@
+//
+//  BBCommonTests.m
+//  BBCommonTests
+//
+//  Created by Brian Romanko on 9/18/11.
+//  Copyright 2011 BigBig Bomb, LLC. All rights reserved.
+//
+
+#import "BBCommonTests.h"
+
+
+@implementation BBCommonTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BBCommonTests");
+}
+
+@end
