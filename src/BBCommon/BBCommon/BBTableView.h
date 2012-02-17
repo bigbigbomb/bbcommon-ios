e6bddfd88092634f3d57b4f87f677f2a31ddb2af
Lee Fastenau <fast@bigbigbomb.com>
2012-02-17 15:30:39 -0600

    BBTableView support for adding a bottom fill color (basis for extending UITableView in other useful way).

---
+++ src/BBCommon/BBCommon/BBTableView.h
@@ -0,0 +1,12 @@
+//
+//  Created by Lee Fastenau on 2/17/12.
+//  Copyright 2011 BigBig Bomb, LLC. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+
+@interface BBTableView : UITableView {
+}
+
+@property(nonatomic, retain) UIColor *bottomFillColor;
+@end
---
+++ src/BBCommon/BBCommon/BBTableView.m
@@ -0,0 +1,38 @@
+//
+//  Created by Lee Fastenau on 2/17/12.
+//  Copyright 2011 BigBig Bomb, LLC. All rights reserved.
+//
+#import "BBTableView.h"
+#import "UIView+BBCommon.h"
+
+@implementation BBTableView {
+@private
+    UIColor *_bottomFillColor;
+}
+
+@synthesize bottomFillColor = _bottomFillColor;
+
+- (void)drawRect:(CGRect)rect {
+    CGContextRef myContext = UIGraphicsGetCurrentContext();
+
+    float fillHeight = BBH(self) - (self.contentSize.height - self.contentOffset.y + self.contentInset.bottom);
+
+    if (_bottomFillColor != nil && fillHeight > 0) {
+
+        CGContextSetFillColorWithColor(myContext, [_bottomFillColor CGColor]);
+        CGContextFillRect(myContext, CGRectMake(0, self.contentSize.height, BBW(self), fillHeight));
+    }
+}
+
+- (void)setContentOffset:(CGPoint)aContentOffset {
+    [super setContentOffset:aContentOffset];
+    [self setNeedsDisplay];
+}
+
+
+- (void)dealloc {
+    [_bottomFillColor release];
+    [super dealloc];
+}
+
+@end
