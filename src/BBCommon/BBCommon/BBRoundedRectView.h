89af58ea95674222878b5e65db63ca215ab54143
Lee Fastenau <fast@bigbigbomb.com>
2012-02-24 17:37:59 -0600

    Added rounded rect view.

---
+++ src/BBCommon/BBCommon/BBRoundedRectView.h
@@ -0,0 +1,11 @@
+//
+//  Created by Lee Fastenau on 2/24/12.
+//  Copyright 2011 BigBig Bomb, LLC. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+
+@interface BBRoundedRectView : UIView
+@property(nonatomic, retain) UIColor *color;
+@property(nonatomic) CGSize cornerRadii;
+@end
---
+++ src/BBCommon/BBCommon/BBRoundedRectView.m
@@ -0,0 +1,55 @@
+//
+//  Created by Lee Fastenau on 2/24/12.
+//  Copyright 2011 BigBig Bomb, LLC. All rights reserved.
+//
+#import "BBRoundedRectView.h"
+
+
+@implementation BBRoundedRectView {
+
+@private
+    UIColor *_color;
+    CGSize _cornerRadii;
+}
+@synthesize color = _color;
+@synthesize cornerRadii = _cornerRadii;
+
+- (void)setColor:(UIColor *)aColor {
+    NonatomicRetainedSetToFrom(_color, aColor);
+    [self setNeedsDisplay];
+}
+
+- (void)setCornerRadii:(CGSize)aCornerRadii {
+    _cornerRadii = aCornerRadii;
+    [self setNeedsDisplay];
+}
+
+- (void)drawRect:(CGRect)rect {
+    UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:rect byRoundingCorners:UIRectCornerAllCorners cornerRadii:self.cornerRadii];
+    [self.color setFill];
+    [path fill];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    [self setNeedsDisplay];
+}
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor clearColor];
+        [self addObserver:self forKeyPath:@"frame" options:NSKeyValueObservingOptionNew context:nil];
+    }
+
+    return self;
+}
+
+
+- (void)dealloc {
+    [self removeObserver:self forKeyPath:@"frame"];
+    [_color release];
+    [super dealloc];
+}
+
+
+@end
