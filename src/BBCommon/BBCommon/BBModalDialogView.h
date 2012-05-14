557b9ee298a3bc755ab5200cf7b84a93843cf8aa
Brian Romanko <brian@bigbigbomb.com>
2012-05-14 11:13:24 -0500

    Adding BBModalDialogView

---
+++ src/BBCommon/BBCommon/BBModalDialogView.h
@@ -0,0 +1,16 @@
+//
+//  Created by Lee Fastenau on 2/24/12.
+//  Copyright 2011 BigBig Bomb, LLC. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+
+@interface BBModalDialogView : UIView
+
+@property(nonatomic, retain) UIView *contentView;
+
+- (void)setContentView:(UIView *)contentView animated:(BOOL)animated;
+
++ (void)presentDialog:(UIView *)view delay:(NSTimeInterval)delay block:(void (^)())block;
+
+@end
